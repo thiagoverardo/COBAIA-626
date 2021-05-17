93943eb8ca7e67cdd3783bc876e53b636d24be5d
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-05-17 09:39:48 -0300

    pause e melhor movimentacao objetos

---
+++ FirstPersonPuzzle/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool paused = false;
+    public GameObject pauseMenu;
+    void Start()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                Time.timeScale = 0;
+                paused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                UnpauseGame();
+            }
+
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
