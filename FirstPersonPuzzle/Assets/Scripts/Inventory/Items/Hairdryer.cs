de4c357304f4fa495a84f5944d1357c432d298ca
unknown <thiagovcs@al.insper.edu.br>
2021-06-02 13:38:57 -0300

    hairDryer

---
+++ FirstPersonPuzzle/Assets/Scripts/Inventory/Items/Hairdryer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hairdryer : InventoryItemBase
+{
+    public override string Name
+    {
+        get
+        {
+            return "hairdryer";
+        }
+    }
+
+    public override void OnUse()
+    {
+        base.OnUse();
+    }
+}
